f365a861d3897c9454015483d2a5c4d556f75f0a
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-07-23 17:27:03 -0700

    corrected

--- app/js/hamburger.js
+++ app/js/hamburger.js
@@ -7,11 +7,6 @@ function mobileMenu() {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 }
-// const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav__link');
 
-// navLink.forEach((n) => n.addEventListener('click', closeMenu));
-
-function closeMenu() {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-}
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
